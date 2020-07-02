f897dc6cb1d17d802131bfbdc622e1ddff304d83
nanou <nanou@172.16.134.206>
2020-07-02 18:32:42 +0200

    Added imports

--- Munchkin/src/presentation/FenetrePrincipale.java
+++ Munchkin/src/presentation/FenetrePrincipale.java
@@ -1,5 +1,25 @@
 package presentation;
 
-public class FenetrePrincipale {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+
+public class FenetrePrincipale extends JFrame{
 
 }
