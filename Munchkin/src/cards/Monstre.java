1e01d15f04101b73847100a6ec6fa01c75f90d7b
thibaut <thibaut@172.16.132.146>
2020-07-02 17:08:19 +0200

    Monstre avec get et set

--- Munchkin/src/cards/Monstre.java
+++ Munchkin/src/cards/Monstre.java
@@ -4,8 +4,43 @@ public class Monstre {
 	private String Nom;
 	private Integer Force;
 	private Integer NbrNiveau;
-	private Integer Nbrtresor;
+	private Integer NbrTresor;
 	private String IncidentFacheux;
-	private Integer Bonus;
+	private String Description;
+	
+	public Monstre(String Nom,Integer Force,Integer NbrNiveau,Integer NbrTresor,String IncidentFacheux,String Description) {
+		this.Nom = Nom;
+		this.Force = Force;
+		this.NbrTresor = NbrTresor;
+		this.NbrNiveau = NbrNiveau;
+		this.IncidentFacheux = IncidentFacheux;
+		this.Description = Description;
+	}
+
+	public Integer getForce() {
+		return Force;
+	}
+
+	public void setForce(Integer force) {
+		Force = force;
+	}
+
+	public Integer getNbrNiveau() {
+		return NbrNiveau;
+	}
+
+	public void setNbrNiveau(Integer nbrNiveau) {
+		NbrNiveau = nbrNiveau;
+	}
+
+	public Integer getNbrTresor() {
+		return NbrTresor;
+	}
+
+	public void setNbrTresor(Integer nbrTresor) {
+		NbrTresor = nbrTresor;
+	}
+
+	
 	
 }
