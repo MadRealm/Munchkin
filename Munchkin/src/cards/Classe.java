cf4254e12a255d5e175fa8bf1d0b54c64b37e78b
thibaut <thibaut@172.16.132.146>
2020-07-03 19:21:29 +0200

    gh

--- Munchkin/src/cards/Classe.java
+++ Munchkin/src/cards/Classe.java
@@ -1,8 +1,27 @@
 package cards;
 
+import presentation.Joueur;
+
 public enum Classe {
-    sans_classe;
+    sans_classe(true),
+	guerrier(true),
+	voleur(true),
+	magicien(true),
+	prêtre(true);
 	
+	private boolean Equipable;
+	
+	Classe classe = Joueur.getClasse();
+	
+	Classe(boolean Equipable){
+		this.Equipable = Equipable;
+	}
+	
+	public boolean EstEquipable() {
+		if(Joueur.getClasse() == sans_classe) {
+			return true;
+		}
+	}
 }
 
 
